4570ebf0e7076be6791c5aae86fcc3c355dd796a
Eric Moura <ericmoura@hotmail.com>
2015-03-11 01:04:01 -0400

    QLearning Agent Begin

--- Q Learning Pacman/lispfunctions.lisp
+++ Q Learning Pacman/lispfunctions.lisp
@@ -1,6 +1,5 @@
 (defparameter javaobj 0)
 
-
 ;; JAVA-CONNECTION Functions
 (defun init(param)
   (setf javaobj param)
@@ -49,4 +48,51 @@
 (defun test()
   (call-java "test" (list-to-string '(1 2 3 4)))
 )
+
+;;Qlearning
+
+(defparameter epsilon 0.05) ;; learning rate
+(defparameter gamma 0.8);; exploration rate
+(defparameter alpha 0.2);; discount factor
+(defparameter weights '())
+
+
+;;Compute the action to take in the current state.  With
+;;probability epsilon, we should take a random action and
+;;take the best policy action otherwise.  Note that if there are
+;;no legal actions the return is nil
+(defun get-action(state)
+  (if (flip-coin (epsilon)) 
+    (get-random-action(get-list-of-actions(state))) 
+    (compute-action-from-qvalues(state))
+  )
+)
+
+;;update your weights based on transition
+(defun update()
+  
+)
+
+;;return Q(state,action) = w * featureVector
+(defun get-qvalue(state, action)
+  (apply '+ (mapcar #'* weights (get-features(state, action))))
+)
+
+(defun flip-coin (e)
+  (< (random 100) e*100) 
+)
+
+(defun get-random-action(actions)
+  (nth (random (lenght actions)) actions)
+)
+
+(defun compute-action-from-qvalues (state)
+  (setf actions (get-list-of-actions(state)))
+  (setf best-action '())
+  ;;choose best action based in the getQValue(state, action)
+  ;;TODO
+)
+
+
+
 
